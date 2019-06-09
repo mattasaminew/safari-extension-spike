106db325a96d6e614879c82f72cb3f7022d3b18f
“mattasaminew” <mattasaminew@gmail.com>
2019-06-09 12:12:38 -0400

    update embedded plugin

--- PlugIns/Shopbrain Extension.appex/Contents/Resources/script.js
+++ PlugIns/Shopbrain Extension.appex/Contents/Resources/script.js
@@ -1,23 +1,61 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-                          
-  var title = document.querySelector("#productTitle").innerText
-  console.log(title)
-  console.log("Safari runtime: ", safari)
-  safari.extension.dispatchMessage(title);
+  safari.extension.dispatchMessage("extensionLoaded")
+  const itemName = document.querySelector("#productTitle").innerText
+  console.log("Product title: ", itemName)
                           
   if (window.top === window) {
     const elem = document.createElement("div")
     elem.innerText = "HELLO WORLD"
     const firstChild = document.querySelector('body').firstChild
     document.querySelector('body').insertBefore(elem, firstChild)
-    // The parent frame is the top-level frame, not an iframe.
-    // All non-iframe code goes before the closing brace.
   }
+                          
+  safari.self.addEventListener("message", function(event) {
+    console.log("EVENT: ", event)
+    if (event.name === "iconClick") {
+      safari.extension.dispatchMessage("amazonPageInfo", {"itemName": itemName})
+    }
+    if (event.name === "resultFound") {
+      console.log(event)
+      console.log(event.message.searchResults)
+      const container = document.createElement("div")
+      container.style.position = "absolute"
+      container.style.top = "0"
+      container.style.right = "0"
+      container.style.width = "30%"
+      container.style.zIndex = "1000"
+      container.style.backgroundColor = "white"
+                               
+      const searchResults = event.message.searchResults
+      searchResults.forEach((result) => {
+        console.log("Result", result)
+        const item = document.createElement("div")
+                               
+        const image = document.createElement("img")
+        image.setAttribute("src", result.img)
+        
+        const name = document.createElement("h2")
+        name.innerText = result.name
+        
+        const price = document.createElement("p")
+        price.innerText = result.price
+                               
+        item.append(image)
+        item.append(name)
+        item.append(price)
+                               
+        container.append(item)
+      })
+      
+      const firstChild = document.querySelector('body').firstChild
+      document.querySelector('body').insertBefore(container, firstChild)
+//      document.insertBefore(container)
+      // inject results into window
+    }
+  })
 });
 
-safari.self.addEventListener("message", function(event) {
-  console.log("EVENT: ", event)
-})
+
 
 
 
